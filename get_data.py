541423ef55ddc876489e30f6388ba650aa1227ba
denbergvanthijs <denbergvanthijs@gmail.com>
2020-10-07 15:42:43 +0200

    Loads test data from seperate file

--- get_data.py
+++ get_data.py
@@ -8,7 +8,7 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 def load_image(data_source: str):
     """
     Loads one of the following image datasets: {mnist, famnist, cifar10}.
-    Normalizes the data. Returns X and y.
+    Normalizes the data. Returns X and y for both train and test datasets.
     """
     reshape_shape = -1, 28, 28, 1
 
@@ -25,77 +25,78 @@ def load_image(data_source: str):
     else:
         raise ValueError("No valid `data_source`.")
 
-    X = np.concatenate((X_train, X_test))  # Combine train/test to make new train/test/validate later on
-    y = np.concatenate((y_train, y_test))
+    X_train = X_train.reshape(reshape_shape)
+    X_test = X_test.reshape(reshape_shape)
 
-    X = X.reshape(reshape_shape)
-    X = X / 255  # /= is not available when casting int to float: https://stackoverflow.com/a/48948461/10603874
-    y = y.reshape(y.shape[0], )
+    X_train = X_train / 255  # /= is not available when casting int to float: https://stackoverflow.com/a/48948461/10603874
+    X_test = X_test / 255
 
-    return X, y
+    y_train = y_train.reshape(y_train.shape[0], )
+    y_test = y_test.reshape(y_test.shape[0], )
+
+    return X_train, y_train, X_test, y_test
 
 
 def load_imdb(config=(5_000, 500)):
-    """Loads the IMDB dataset. Returns X, y."""
+    """Loads the IMDB dataset. Returns X and y for both train and test datasets."""
     (X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=config[0])
-    X = np.concatenate((X_train, X_test))  # Combine train/test to make new train/test/validate later on
-    y = np.concatenate((y_train, y_test))
 
-    X = pad_sequences(X, maxlen=config[1])
+    X_train = pad_sequences(X_train, maxlen=config[1])
+    X_test = pad_sequences(X_test, maxlen=config[1])
 
-    return X, y
+    return X_train, y_train, X_test, y_test
 
 
-def load_creditcard(fp: str = "./data/creditcard.csv"):
+def load_creditcard(fp_train: str = "./data/credit0.csv", fp_test: str = "./data/credit1.csv", normalization: bool = False):
     """
-    Loads the Kaggle Credit Card Fraud dataset from local filepath. Returns X, y.
+    Loads the Kaggle Credit Card Fraud dataset from local filepath. Returns X and y for both train and test datasets.
     Source: https://www.kaggle.com/mlg-ulb/creditcardfraud
     """
-    X = read_csv(fp)  # Directly converted to float64
+    X_train = read_csv(fp_train)  # Directly converted to float64
+    X_test = read_csv(fp_test)
 
-    y = X["Class"]  # 1: Fraud/Minority, 0: No fraud/Majority
-    X.drop(columns=["Time", "Class"], inplace=True)  # Dropping `Time` since future data for the model could have another epoch
-    X = X.astype(np.float32)
+    y_train = X_train["Class"]  # 1: Fraud/Minority, 0: No fraud/Majority
+    y_test = X_test["Class"]
+    X_train.drop(columns=["Time", "Class"], inplace=True)  # Dropping `Time` since future data for the model could have another epoch
+    X_test.drop(columns=["Time", "Class"], inplace=True)
+    X_train = X_train.astype(np.float32)
+    X_test = X_test.astype(np.float32)
 
-    return X.values, y.values  # Numpy arrays
+    # Other data sources are already normalized. RGB values are always in range 0 to 255.
+    if normalization:
+        mini, maxi = X_train.min(axis=0), X_train.max(axis=0)
+        X_train -= mini
+        X_train /= maxi-mini
+        X_test -= mini
+        X_test /= maxi-mini
+
+    return X_train.values, y_train.values, X_test.values, y_test.values  # Numpy arrays
 
 
-def load_data(data_source: str, imb_rate: float, min_class: list, maj_class: list,
-              seed: float = None, normalization: bool = False, print_stats: bool = True):
+def load_data(data_source: str, imb_rate: float, min_class: list, maj_class: list, normalization: bool = False, print_stats: bool = True):
     """
     Loads data from the `data_source`. Imbalances the data and divides the data into train, test and validation sets.
     The imbalance rate of each individual dataset is the same as the given `imb_rate`.
     The seed for the test dataset is already set to ensure test data is always the same.
     """
     if data_source in ("famnist", "mnist", "cifar10"):
-        X, y = load_image(data_source=data_source)
+        X_train, y_train, X_test, y_test = load_image(data_source=data_source)
     elif data_source == "credit":
-        X, y = load_creditcard()
+        X_train, y_train, X_test, y_test = load_creditcard(normalization=normalization)
     elif data_source == "imdb":
-        X, y = load_imdb()
+        X_train, y_train, X_test, y_test = load_imdb()
     else:
         raise ValueError("No valid `data_source`.")
 
-    X_imb, y_imb = get_imb_data(X, y, imb_rate, min_class, maj_class)  # Imbalance the data
+    X_train, y_train = get_imb_data(X_train, y_train, imb_rate, min_class, maj_class)  # Imbalance the data
+    X_test, y_test = get_imb_data(X_test, y_test, imb_rate, min_class, maj_class)  # Imbalance the data
 
     # 60 / 20 / 20 for train / test / validate; stratify=y to ensure class balance is kept
-    # Seed for train / test is always 42 to ensure test data is always the same
-    # Seed for train / validate is not set to ensure random split
-    X_rest, X_test, y_rest, y_test = train_test_split(X_imb, y_imb, test_size=0.2, random_state=42, stratify=y_imb)
-    X_train, X_val, y_train, y_val = train_test_split(X_rest, y_rest, test_size=0.25, random_state=seed, stratify=y_rest)
-
-    if data_source == "credit" and normalization:
-        # Normalize data. This does not happen in load_creditcard to prevent train/test/val leakage
-        # Other data sources are already normalized. RGB values are always in range 0 to 255.
-        mini, maxi = X_train.min(axis=0), X_train.max(axis=0)
-        for X in (X_train, X_test, X_val):  # Normalize to min-max
-            X -= mini
-            X /= maxi-mini
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.25, stratify=y_train)
 
     if print_stats:
-        p_data, p_train, p_test, p_val = [((y == 1).sum(), (y == 1).sum() / (y == 0).sum()) for y in (y_imb, y_train, y_test, y_val)]
+        p_train, p_test, p_val = [((y == 1).sum(), (y == 1).sum() / (y == 0).sum()) for y in (y_train, y_test, y_val)]
         print(f"Imbalance ratio `p`:\n"
-              f"\tdataset:    n={p_data[0]}, p={p_data[1]:.6f}\n"
               f"\ttrain:      n={p_train[0]}, p={p_train[1]:.6f}\n"
               f"\ttest:       n={p_test[0]}, p={p_test[1]:.6f}\n"
               f"\tvalidation: n={p_val[0]}, p={p_val[1]:.6f}")
@@ -133,4 +134,4 @@ def get_imb_data(X, y, imb_rate: float, min_class: list, maj_class: list):
 
 if __name__ == "__main__":
     X_train, y_train, X_test, y_test, X_val, y_val = load_data("credit", 0.00173, [1], [0])
-    print([i.shape for i in (X_train, y_train, X_test, y_test, X_val, y_val)])
+    print(*[i.shape for i in (X_train, y_train, X_test, y_test, X_val, y_val)])
