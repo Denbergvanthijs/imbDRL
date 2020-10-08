b80bcf62028b54313334fcfe187b3093b887ebd9
denbergvanthijs <denbergvanthijs@gmail.com>
2020-10-08 16:37:40 +0200

    Changed dtypes to float32 and int32

--- get_data.py
+++ get_data.py
@@ -3,6 +3,7 @@ from pandas import read_csv
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.datasets import cifar10, fashion_mnist, imdb, mnist
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.python.keras.backend import dtype
 
 
 def load_image(data_source: str):
@@ -25,14 +26,14 @@ def load_image(data_source: str):
     else:
         raise ValueError("No valid `data_source`.")
 
-    X_train = X_train.reshape(reshape_shape)
-    X_test = X_test.reshape(reshape_shape)
+    X_train = X_train.reshape(reshape_shape).astype(np.float32)  # Float32 is the expected dtype for the observation spec in the env
+    X_test = X_test.reshape(reshape_shape).astype(np.float32)
 
-    X_train = X_train / 255  # /= is not available when casting int to float: https://stackoverflow.com/a/48948461/10603874
-    X_test = X_test / 255
+    X_train /= 255  # /= is not available when casting int to float: https://stackoverflow.com/a/48948461/10603874
+    X_test /= 255
 
-    y_train = y_train.reshape(y_train.shape[0], )
-    y_test = y_test.reshape(y_test.shape[0], )
+    y_train = y_train.reshape(y_train.shape[0], ).astype(np.int32)
+    y_test = y_test.reshape(y_test.shape[0], ).astype(np.int32)
 
     return X_train, y_train, X_test, y_test
 
@@ -52,15 +53,13 @@ def load_creditcard(fp_train: str = "./data/credit0.csv", fp_test: str = "./data
     Loads the Kaggle Credit Card Fraud dataset from local filepath. Returns X and y for both train and test datasets.
     Source: https://www.kaggle.com/mlg-ulb/creditcardfraud
     """
-    X_train = read_csv(fp_train)  # Directly converted to float64
-    X_test = read_csv(fp_test)
+    X_train = read_csv(fp_train).astype(np.float32)  # DataFrames directly converted to float32
+    X_test = read_csv(fp_test).astype(np.float32)
 
-    y_train = X_train["Class"]  # 1: Fraud/Minority, 0: No fraud/Majority
-    y_test = X_test["Class"]
+    y_train = X_train["Class"].astype(np.int32)  # 1: Fraud/Minority, 0: No fraud/Majority
+    y_test = X_test["Class"].astype(np.int32)
     X_train.drop(columns=["Time", "Class"], inplace=True)  # Dropping `Time` since future data for the model could have another epoch
     X_test.drop(columns=["Time", "Class"], inplace=True)
-    X_train = X_train.astype(np.float32)
-    X_test = X_test.astype(np.float32)
 
     # Other data sources are already normalized. RGB values are always in range 0 to 255.
     if normalization:
@@ -122,16 +121,16 @@ def get_imb_data(X, y, imb_rate: float, min_class: list, maj_class: list):
         if value in maj_class:
             X_maj.append(X[i])
 
-    X_maj_len = len(X_maj)
-    min_len = int(X_maj_len * imb_rate)
+    min_len = int((X_maj_len := len(X_maj)) * imb_rate)
 
     # Keep all majority rows, decrease minority rows to match `imb_rate`
-    X_imb = np.array(X_maj + X_min[:min_len])  # `min_len` can be more than the number of minority rows
-    y_imb = np.concatenate((np.zeros(X_maj_len), np.ones(X_imb.shape[0] - X_maj_len)))
+    X_imb = np.array(X_maj + X_min[:min_len], dtype=np.float32)  # `min_len` can be more than the number of minority rows
+    y_imb = np.concatenate((np.zeros(X_maj_len), np.ones(X_imb.shape[0] - X_maj_len))).astype(np.int32)
 
     return X_imb, y_imb
 
 
 if __name__ == "__main__":
-    X_train, y_train, X_test, y_test, X_val, y_val = load_data("credit", 0.00173, [1], [0])
+    X_train, y_train, X_test, y_test, X_val, y_val = load_data("mnist", 0.05, [3], [8])
+    print(*[i.dtype for i in (X_train, y_train, X_test, y_test, X_val, y_val)])
     print(*[i.shape for i in (X_train, y_train, X_test, y_test, X_val, y_val)])
