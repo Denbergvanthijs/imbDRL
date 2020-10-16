2c664689edad7b8f9604483d4cd20be01a2dff5b
denbergvanthijs <denbergvanthijs@gmail.com>
2020-10-16 14:45:24 +0200

    Improved metrics for bandit on MNIST

--- dqnimp/examples/train_bandit.py
+++ dqnimp/examples/train_bandit.py
@@ -1,72 +1,77 @@
-import functools
 from datetime import datetime
 
+import numpy as np
 import tensorflow as tf
 import tensorflow_probability as tfp
 from dqnimp.data import load_data
-from dqnimp.utils import metrics_by_network
+from dqnimp.metrics import metrics_by_network
 from tensorflow.keras.optimizers import Adam
-from tf_agents.bandits.agents import neural_epsilon_greedy_agent
 from tf_agents.bandits.agents.examples.v2.trainer import (get_replay_buffer,
                                                           get_training_loop_fn)
-from tf_agents.bandits.environments import classification_environment as ce
-from tf_agents.bandits.environments import environment_utilities as env_util
-from tf_agents.drivers import dynamic_step_driver
+from tf_agents.bandits.agents.neural_epsilon_greedy_agent import \
+    NeuralEpsilonGreedyAgent
+from tf_agents.bandits.environments.classification_environment import \
+    ClassificationBanditEnvironment
+from tf_agents.drivers.dynamic_step_driver import DynamicStepDriver
 from tf_agents.metrics import tf_metrics
-from tf_agents.networks import q_network
+from tf_agents.networks.q_network import QNetwork
 from tqdm import tqdm
 
 tf.compat.v1.enable_v2_behavior()  # The trainer only runs with V2 enabled.
-BATCH_SIZE = 100
-TRAINING_LOOPS = 1_000
-STEPS_PER_LOOP = 4
+batch_size = 64
+training_loops = 1_000
+steps_per_loop = 64
+
+conv_layers = ((32, (5, 5), 2), (32, (5, 5), 2), )
+dense_layers = (256, )
+dropout_layers = None
+
+lr = 0.001  # Learning Rate
+min_epsilon = 0.01  # Minimal chance of choosing random action
+decay_episodes = 250  # Number of episodes to decay from 1.0 to `min_epsilon`, divided by 4
+global_step = tf.Variable(0, name="global_step", dtype=np.int64, trainable=False)  # Global train episode counter
+eps_decay = tf.compat.v1.train.polynomial_decay(1.0, global_step, decay_episodes, end_learning_rate=min_epsilon)
+
 log_dir = "./logs/" + (NOW := datetime.now().strftime('%Y%m%d_%H%M%S'))
 
-imb_rate = 0.05  # Imbalance rate
-min_class = [3]  # Minority classes, must be same as trained model
-maj_class = [8]  # Majority classes, must be same as trained model
+imb_rate = 0.01  # Imbalance rate
+min_class = [2]  # Minority classes, same setup as in original paper
+maj_class = [0, 1, 3, 4, 5, 6, 7, 8, 9]  # Majority classes
 datasource = "mnist"  # The dataset to be selected
 X_train, y_train, X_test, y_test, X_val, y_val = load_data(datasource, imb_rate, min_class, maj_class, normalization=True)
 
-with tf.device('/CPU:0'):  # due to b/128333994
+with tf.device('/GPU:0'):  # due to b/128333994
     distr = tfp.distributions.Bernoulli(probs=[[imb_rate, -imb_rate], [-1, 1]], dtype=tf.float32)
     reward_distr = (tfp.bijectors.Shift([[imb_rate, -imb_rate], [-1, 1]])
                     (tfp.bijectors.Scale([[1, 1], [1, 1]])
                      (distr)))
     distr = tfp.distributions.Independent(reward_distr, reinterpreted_batch_ndims=2)
     dataset = tf.data.Dataset.from_tensor_slices((X_train, y_train))
+    environment = ClassificationBanditEnvironment(dataset, distr, batch_size)
 
-    environment = ce.ClassificationBanditEnvironment(dataset, distr, BATCH_SIZE)
-    optimal_reward_fn = functools.partial(env_util.compute_optimal_reward_with_classification_environment, environment=environment)
-    optimal_action_fn = functools.partial(env_util.compute_optimal_action_with_classification_environment, environment=environment)
+    network = QNetwork(input_tensor_spec=environment.observation_spec(),
+                       action_spec=environment.action_spec(),
+                       conv_layer_params=conv_layers,
+                       fc_layer_params=dense_layers)
 
-    network = q_network.QNetwork(input_tensor_spec=environment.observation_spec(),
-                                 action_spec=environment.action_spec(),
-                                 conv_layer_params=((32, (5, 5), 2), (32, (5, 5), 2), ),
-                                 fc_layer_params=(256, 128))
+    agent = NeuralEpsilonGreedyAgent(time_step_spec=environment.time_step_spec(),
+                                     action_spec=environment.action_spec(),
+                                     reward_network=network,
+                                     optimizer=Adam(learning_rate=lr),
+                                     epsilon=eps_decay,
+                                     train_step_counter=global_step)
 
-    agent = neural_epsilon_greedy_agent.NeuralEpsilonGreedyAgent(time_step_spec=environment.time_step_spec(),
-                                                                 action_spec=environment.action_spec(),
-                                                                 reward_network=network,
-                                                                 optimizer=Adam(learning_rate=0.0005),
-                                                                 epsilon=0.01)
-    data_spec = agent.policy.trajectory_spec
-    replay_buffer = get_replay_buffer(data_spec, environment.batch_size, STEPS_PER_LOOP)
+    replay_buffer = get_replay_buffer(agent.policy.trajectory_spec, environment.batch_size, steps_per_loop)
 
-    step_metric = tf_metrics.EnvironmentSteps()
-    add_batch_fn = replay_buffer.add_batch
+    driver = DynamicStepDriver(env=environment,
+                               policy=agent.collect_policy,
+                               num_steps=steps_per_loop * environment.batch_size,
+                               observers=[replay_buffer.add_batch])
 
-    observers = [add_batch_fn, step_metric]
-
-    driver = dynamic_step_driver.DynamicStepDriver(env=environment,
-                                                   policy=agent.collect_policy,
-                                                   num_steps=STEPS_PER_LOOP * environment.batch_size,
-                                                   observers=observers)
-
-    training_loop = get_training_loop_fn(driver, replay_buffer, agent, STEPS_PER_LOOP)
+    training_loop = get_training_loop_fn(driver, replay_buffer, agent, steps_per_loop)
     writer = tf.summary.create_file_writer(log_dir)
 
-    for i in tqdm(range(TRAINING_LOOPS + 1)):
+    for i in tqdm(range(training_loops + 1)):
         training_loop()
 
         if not i % 20:
