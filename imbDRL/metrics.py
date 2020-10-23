5fd5f300ed65c06d9bf2b4a66931032626ea09d4
denbergvanthijs <denbergvanthijs@gmail.com>
2020-10-23 17:01:34 +0200

    Added and improved tests for coverage

--- imbDRL/metrics.py
+++ imbDRL/metrics.py
@@ -1,68 +1,24 @@
 import numpy as np
-import tensorflow as tf
 from sklearn.metrics import confusion_matrix, f1_score, fbeta_score
-from tf_agents.trajectories import time_step as ts
 
 
-def metrics_by_network(network, X: np.ndarray, y: np.ndarray) -> dict:
-    """Computes metrics using a given network.
-    Input is X and y data. X will be used to make y_pred.
-    y_pred and y will be compared using `imbDRL.metrics.classification_metrics()`.
+def network_predictions(network, X: np.ndarray) -> dict:
+    """Computes y_pred using a given network.
+    Input is array of data entries.
 
     :param network: The network to use to calculate metrics
     :type  network: (Q)Network
-    :param X: X data
+    :param X: X data, input to network
     :type  X: np.ndarray
-    :param y: y data, labels corresponding to X
-    :type  y: np.ndarray
 
-    :return: Dictionairy containing Geometric Mean, F0.5, F1, F2, TP, TN, FP, FN
-    :rtype: dict
+    :return: Numpy array of predicted targets for given X
+    :rtype: np.ndarray
     """
     if not isinstance(X, np.ndarray):
         raise ValueError(f"`X` must be of type `np.ndarray` not {type(X)}")
-    if not isinstance(y, np.ndarray):
-        raise ValueError(f"`y` must be of type `np.ndarray` not {type(y)}")
-    if X.shape[0] != y.shape[0]:
-        raise ValueError("`X` and `y` must contain the same amount of rows.")
 
     q, _ = network(X)
-    y_pred = np.argmax(q.numpy(), axis=1)  # Max action for each x in X
-
-    return classification_metrics(y, y_pred)
-
-
-def metrics_by_policy(policy, X: list, y: list) -> dict:
-    """Computes metrics using a given policy.
-    Input is X and y data. X will be used to make y_pred.
-    y_pred and y will be compared using `imbDRL.metrics.classification_metrics()`.
-
-    :param policy: The policy to use to calculate metrics
-    :type  policy: policy
-    :param X: X data
-    :type  X: np.ndarray
-    :param y: y data, labels corresponding to X
-    :type  y: np.ndarray
-
-    :return: Dictionairy containing Geometric Mean, F0.5, F1, F2, TP, TN, FP, FN
-    :rtype: dict
-    """
-    if not isinstance(X, np.ndarray):
-        raise ValueError(f"`X` must be of type `np.ndarray` not {type(X)}")
-    if not isinstance(y, np.ndarray):
-        raise ValueError(f"`y` must be of type `np.ndarray` not {type(y)}")
-    if X.shape[0] != y.shape[0]:
-        raise ValueError("`X` and `y` must contain the same amount of rows.")
-
-    y_pred = []
-    shape = (1, ) + X.shape[1:]
-    for x in X:
-        tf_x = tf.constant(x, shape=shape)
-        time_step = ts.transition(observation=tf_x, reward=0.0, outer_dims=shape)
-        action_step = policy.action(time_step)
-        y_pred.append(action_step.action)
-
-    return classification_metrics(y, tf.stack(y_pred))
+    return np.argmax(q.numpy(), axis=1)  # Max action for each x in X
 
 
 def classification_metrics(y_true: list, y_pred: list) -> dict:
--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,54 +1,29 @@
+from tf_agents.policies.random_tf_policy import RandomTFPolicy
+from imbDRL.environments import ClassifyEnv
+from tf_agents.environments.tf_py_environment import TFPyEnvironment
 import imbDRL.metrics as metrics
 import numpy as np
 import pytest
+import tensorflow as tf
 
 
-def test_metrics_by_network():
-    """Tests imbDRL.metrics.metrics_by_network."""
+def test_network_predictions():
+    """Tests imbDRL.metrics.network_predictions."""
     X = [7, 7, 7, 8, 8, 8]
-    y = [2, 2, 2, 3, 3, 3]
 
     with pytest.raises(ValueError) as exc:
-        metrics.metrics_by_network([], X, np.array(y))
+        metrics.network_predictions([], X)
     assert "`X` must be of type" in str(exc.value)
 
-    with pytest.raises(ValueError) as exc:
-        metrics.metrics_by_network([], np.array(X), y)
-    assert "`y` must be of type" in str(exc.value)
-
-    X = np.arange(10)
-    y = np.arange(11)
-
-    with pytest.raises(ValueError) as exc:
-        metrics.metrics_by_network([], X, y)
-    assert "must contain the same amount of rows" in str(exc.value)
-
-
-def test_metrics_by_policy():
-    """Tests imbDRL.metrics.metrics_by_policy."""
-    X = [7, 7, 7, 8, 8, 8]
-    y = [2, 2, 2, 3, 3, 3]
-
-    with pytest.raises(ValueError) as exc:
-        metrics.metrics_by_policy([], X, np.array(y))
-    assert "`X` must be of type" in str(exc.value)
-
-    with pytest.raises(ValueError) as exc:
-        metrics.metrics_by_policy([], np.array(X), y)
-    assert "`y` must be of type" in str(exc.value)
-
-    X = np.arange(10)
-    y = np.arange(11)
-
-    with pytest.raises(ValueError) as exc:
-        metrics.metrics_by_policy([], X, y)
-    assert "must contain the same amount of rows" in str(exc.value)
+    X = np.array([[1, 2], [2, 1], [3, 4], [4, 3]])
+    y_pred = metrics.network_predictions(lambda x: (tf.convert_to_tensor(x), None), X)
+    assert np.array_equal(y_pred, [1, 0, 1, 0])
 
 
 def test_classification_metrics():
     """Tests imbDRL.metrics.classification_metrics."""
-    y_true = [7, 7, 7, 8, 8, 8]
-    y_pred = [2, 2, 2, 3, 3, 3]
+    y_true = [1, 1, 1, 1, 1, 1]
+    y_pred = [1, 1, 1, 0, 0, 0]
 
     with pytest.raises(ValueError) as exc:
         metrics.classification_metrics(1, y_pred)
@@ -61,3 +36,19 @@ def test_classification_metrics():
     with pytest.raises(ValueError) as exc:
         metrics.classification_metrics(y_true, y_pred + [1])
     assert "must be of same length" in str(exc.value)
+
+    stats = metrics.classification_metrics(y_true, y_pred)
+    approx = dict([(k, round(v, 6)) for k, v in stats.items()])
+    assert approx == {"Gmean": 0.0, "Fdot5": 0.833333, "F1": 0.666667, "F2": 0.555556, "TP": 3, "TN": 0, "FP": 0, "FN": 3}
+
+    y_true = [1, 1, 1, 1, 1, 1]
+    y_pred = [0, 0, 0, 0, 0, 0]
+    stats = metrics.classification_metrics(y_true, y_pred)
+    approx = dict([(k, round(v, 6)) for k, v in stats.items()])
+    assert approx == {"Gmean": 0.0, "Fdot5": 0.0, "F1": 0.0, "F2": 0.0, "TP": 0, "TN": 0, "FP": 0, "FN": 6}
+
+    y_true = [0, 0, 0, 0, 0, 0]
+    y_pred = [1, 1, 1, 1, 1, 1]
+    stats = metrics.classification_metrics(y_true, y_pred)
+    approx = dict([(k, round(v, 6)) for k, v in stats.items()])
+    assert approx == {"Gmean": 0.0, "Fdot5": 0.0, "F1": 0.0, "F2": 0.0, "TP": 0, "TN": 0, "FP": 6, "FN": 0}
