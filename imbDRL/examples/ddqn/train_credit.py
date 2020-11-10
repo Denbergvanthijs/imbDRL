11e76e72946d23a7e300dfe13b32e2f7b96cd5c7
denbergvanthijs <denbergvanthijs@gmail.com>
2020-11-10 17:37:47 +0100

    Imrpoved parameters

--- imbDRL/examples/ddqn/train_credit.py
+++ imbDRL/examples/ddqn/train_credit.py
@@ -4,12 +4,12 @@ from imbDRL.examples.ddqn.example_classes import TrainCustomDDQN
 from imbDRL.utils import rounded_dict
 from tf_agents.environments.tf_py_environment import TFPyEnvironment
 
-episodes = 50_000  # Total number of episodes
-warmup_episodes = 50_000  # Amount of warmup steps to collect data with random policy
-memory_length = 50_000  # Max length of the Replay Memory
+episodes = 5_000  # Total number of episodes
+warmup_episodes = 6_000  # Amount of warmup steps to collect data with random policy
+memory_length = warmup_episodes  # Max length of the Replay Memory
 
-target_model_update = 1  # Period to overwrite the target Q-network with the default Q-network  # 500
-target_update_tau = 0.0005  # Soften the target model update  # 0.01
+target_model_update = episodes // 100  # Period to overwrite the target Q-network with the default Q-network
+target_update_tau = 0.005  # Soften the target model update
 
 conv_layers = None  # Convolutional layers
 dense_layers = (256, 256, )  # Dense layers
@@ -24,7 +24,7 @@ imb_rate = 0.001729  # Imbalance rate
 min_class = [1]  # Minority classes
 maj_class = [0]  # Majority classes
 X_train, y_train, X_test, y_test, = load_creditcard(normalization=True)
-X_train, y_train, X_test, y_test, X_val, y_val = get_train_test_val(X_train, y_train, X_test, y_test, imb_rate, min_class, maj_class)
+X_train, y_train, X_test, y_test, X_val, y_val = get_train_test_val(X_train, y_train, X_test, y_test, min_class, maj_class)
 
 # Change Python environment to TF environment
 train_env = TFPyEnvironment(ClassifyEnv(X_train, y_train, imb_rate))
@@ -34,6 +34,6 @@ model = TrainCustomDDQN(episodes, warmup_episodes, lr, gamma, min_epsilon, decay
 
 model.compile_model(train_env, conv_layers, dense_layers, dropout_layers)
 model.train(X_val, y_val)
-stats = model.evaluate(X_test, y_test)
+stats = model.evaluate(X_test, y_test, X_train, y_train)
 print(rounded_dict(stats))
-# ("Gmean", 0.914499) ("Fdot5", 0.756458) ("F1", 0.784689) ("F2", 0.815109) ("TP", 82) ("TN", 56835) ("FP", 29) ("FN", 16)
+# {'Gmean': 0.897701, 'Fdot5': 0.812757, 'F1': 0.810256, 'F2': 0.807771, 'TP': 79, 'TN': 56846, 'FP': 18, 'FN': 19}
