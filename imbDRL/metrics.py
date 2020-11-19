c2c5ad4275c81b2c6944829279ba26db68439526
denbergvanthijs <denbergvanthijs@gmail.com>
2020-11-19 18:24:04 +0100

    Changed metrics to Precision and Recall

--- imbDRL/metrics.py
+++ imbDRL/metrics.py
@@ -50,7 +50,7 @@ def classification_metrics(y_true: list, y_pred: list) -> dict:
     :param y_pred: Predicted labels, corresponding to y_true
     :type  y_pred: np.ndarray
 
-    :return: Dictionairy containing Geometric Mean, F1, Sensitivity, Specificity, TP, TN, FP, FN
+    :return: Dictionairy containing Geometric Mean, F1, Precision, Recall, TP, TN, FP, FN
     :rtype: dict
     """
     if not isinstance(y_true, (list, tuple, np.ndarray)):
@@ -62,12 +62,14 @@ def classification_metrics(y_true: list, y_pred: list) -> dict:
 
     TN, FP, FN, TP = confusion_matrix(y_true, y_pred).ravel()
 
+    precision = TP / denom if (denom := TP + FP) else 0  # Positive predictive value
     recall = TP / denom if (denom := TP + FN) else 0  # Sensitivity, True Positive Rate (TPR)
     specificity = TN / denom if (denom := TN + FP) else 0  # Specificity, selectivity, True Negative Rate (TNR)
+
     G_mean = np.sqrt(recall * specificity)  # Geometric mean of recall and specificity
     F1 = f1_score(y_true, y_pred, zero_division=0)  # Default F-measure
 
-    return {"Gmean": G_mean, "F1": F1, "Sensitivity": recall, "Specificity": specificity, "TP": TP, "TN": TN, "FP": FP, "FN": FN}
+    return {"Gmean": G_mean, "F1": F1, "Precision": precision, "Recall": recall, "TP": TP, "TN": TN, "FP": FP, "FN": FN}
 
 
 def plot_pr_curve(network, X_test: np.ndarray, y_test: np.ndarray,
