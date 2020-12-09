fdfbb35c9e494f2f7db92f2a9f5da89c76b11d55
denbergvanthijs <denbergvanthijs@gmail.com>
2020-12-09 18:00:32 +0100

    Changed supported python versions

--- setup.py
+++ setup.py
@@ -15,11 +15,11 @@ setup(name="imbDRL",
       long_description_content_type="text/markdown",
       url="https://github.com/Denbergvanthijs/imbDRL",
       packages=find_packages(),
-      classifiers=["Programming Language :: Python :: 3.8",
+      classifiers=["Programming Language :: Python :: 3",
                    "License :: OSI Approved :: Apache Software License",
                    "Operating System :: OS Independent",
                    "Environment :: GPU :: NVIDIA CUDA :: 10.1",
                    "Topic :: Scientific/Engineering :: Artificial Intelligence"],
       keywords="imbalanced classification, deep reinforcement learning, deep q-network, reward-function, classification, medical",
       install_requires=install_requires,
-      python_requires=">=3.8")
+      python_requires=">=3.6")
